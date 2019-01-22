40a5d4392800fe85395b0a40a864431b6fc129fc
Tho Nguyen Duc <ndtho8205@gmail.com>
2019-01-22 22:40:03 +0900

    Update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,17 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# build the project
+hugo
 
-# Go To Public folder
+# commit changes, push source and build repos
 cd public
-# Add changes to git.
 git add .
-
-# Commit changes.
 msg="Rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin master
 
-# Come Back up to the Project Root
 cd ..
