df999596ab48796f6924981deea6d28491f8b19d
Tho Nguyen Duc <ndtho8205@gmail.com>
2019-04-03 15:00:34 +0900

    Update baseURL to https

--- content/about.md
+++
@@ -1,9 +0,0 @@
----
-title: 'About'
-Date: '2015-02-01'
----
-
-<div class="about">
-  <h2>What are you looking for here?</h2>
-  <p><strong>GO AWAYYYY! :scream:</strong></p>
-</div>
--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,14 @@ hugo
 
 # commit changes, push source and build repos
 cd public
+rm -rf *
 git add .
 msg="Rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-git push origin master
+git checkout master
+git push origin HEAD:master
 
 cd ..
