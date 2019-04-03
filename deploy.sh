d735f634bb65ff095a1e7c9cc6f370137b921321
Tho Nguyen Duc <ndtho8205@gmail.com>
2019-04-03 15:02:27 +0900

    Fix deploy.sh: delete public folder

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+rm -rf public/*
+
 # build the project
 hugo
 
 # commit changes, push source and build repos
 cd public
-rm -rf *
+
 git add .
 msg="Rebuilding site `date`"
 if [ $# -eq 1 ]
